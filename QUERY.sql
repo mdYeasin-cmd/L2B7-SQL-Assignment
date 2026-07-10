a76aabff2acd1b78eb17d8bd56161e9e879e60bf
mdYeasin-cmd <yeasin200011@gmail.com>
2026-07-10 11:08:14 +0600

    Add Query 3

--- QUERY.sql
+++ QUERY.sql
@@ -11,3 +11,12 @@ SELECT user_id, full_name, email
 FROM users
 WHERE full_name LIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
+
+-- Query 3
+SELECT 
+  booking_id,
+  user_id,
+  match_id,
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
