d3a4295c90956ad7de21f130c41f81ebf072cbfb
mdYeasin-cmd <yeasin200011@gmail.com>
2026-07-10 11:07:23 +0600

    Add Query 2

--- QUERY.sql
+++ QUERY.sql
@@ -5,3 +5,9 @@ SELECT match_id, fixture, base_ticket_price
 FROM matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
+
+-- Query 2
+SELECT user_id, full_name, email
+FROM users
+WHERE full_name LIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
