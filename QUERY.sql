98416eee76eea9152d1ed7809a0e623ff5bfc5c7
mdYeasin-cmd <yeasin200011@gmail.com>
2026-07-10 11:12:29 +0600

    Add Query 7

--- QUERY.sql
+++ QUERY.sql
@@ -50,3 +50,12 @@ WHERE total_cost > (
   SELECT AVG(total_cost)
   FROM bookings
 );
+
+-- Query 7
+SELECT 
+  match_id,
+  fixture,
+  base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
