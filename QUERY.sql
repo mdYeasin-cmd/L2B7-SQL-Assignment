37e7e454904664b5c083f91ea589d2aa9f7506bf
mdYeasin-cmd <yeasin200011@gmail.com>
2026-07-10 23:21:31 +0600

    Remove unnecessary comment

--- QUERY.sql
+++ QUERY.sql
@@ -1,18 +1,13 @@
--- Active: 1783648639069@@127.0.0.1@5432@footballticketbookingsystem
-
--- Query 1
 SELECT match_id, fixture, base_ticket_price
 FROM matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
 
--- Query 2
 SELECT user_id, full_name, email
 FROM users
 WHERE full_name LIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
 
--- Query 3
 SELECT 
   booking_id,
   user_id,
@@ -21,7 +16,6 @@ SELECT
 FROM bookings
 WHERE payment_status IS NULL;
 
--- Query 4
 SELECT 
   b.booking_id,
   u.full_name,
@@ -31,7 +25,6 @@ FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id
 INNER JOIN matches m ON b.match_id = m.match_id;
 
--- Query 5
 SELECT 
   u.user_id,
   u.full_name,
@@ -40,7 +33,6 @@ FROM users u
 LEFT JOIN bookings b ON u.user_id = b.user_id
 ORDER BY u.user_id, b.booking_id;
 
--- Query 6
 SELECT 
   booking_id,
   match_id,
@@ -51,7 +43,6 @@ WHERE total_cost > (
   FROM bookings
 );
 
--- Query 7
 SELECT 
   match_id,
   fixture,
