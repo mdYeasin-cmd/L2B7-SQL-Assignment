a51f203d2f2949d30778b90abfe46ddaaab0a866
mdYeasin-cmd <yeasin200011@gmail.com>
2026-07-10 11:09:47 +0600

    Add Query 5

--- QUERY.sql
+++ QUERY.sql
@@ -30,3 +30,12 @@ SELECT
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id
 INNER JOIN matches m ON b.match_id = m.match_id;
+
+-- Query 5
+SELECT 
+  u.user_id,
+  u.full_name,
+  b.booking_id
+FROM users u
+LEFT JOIN bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
